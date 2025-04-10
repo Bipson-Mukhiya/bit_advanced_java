28400bf170a8ed88a7898910a41539bd7a5fb1d4
Bipson <bipsonmukhiyaxyz@gmail.com>
2025-04-11 00:35:52 +0545

    working with type conversion

---
+++ literels/solution_01.java
@@ -0,0 +1,16 @@
+package literels;
+
+public class solution_01 {
+    public static void main(String[] args) {
+        // literals
+        int num1 = 0b101;
+        double num2 = 56;
+        double num3 = 12e10;
+        char c = 'a';
+        c++;
+        System.out.println(c);
+        System.out.print(num1);
+        System.out.println(num3);
+        System.out.println(num2);
+    }
+}
